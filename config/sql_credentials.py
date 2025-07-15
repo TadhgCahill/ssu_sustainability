21754989de4eeff1bfa61106f5b9fee7e4dc338c
Doctor Laplace <silvervalifor@gmail.com>
2025-07-15 15:40:30 -0700

    Directory Structure Overhaul
    Rearranged and renamed a large amount of files and folders. Created an Automated Watcher to handle automated FTP grabs -> through database updating.

---
+++ config/sql_credentials.py
@@ -0,0 +1,9 @@
+# sql_credentials.py
+SQL_CREDENTIALS = {
+     'user': 'u209446640_SSUTeam',          # database username
+    'password': 'SsuIot!432',               # database password
+    'host': '193.203.166.234',              # database host
+    'database': 'u209446640_SSUEnergy',     # database name
+    'port': 3306,                           #(default is 3306)
+    'connect_timeout': 30
+}
