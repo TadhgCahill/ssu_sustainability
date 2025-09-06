0ba190c134f90c74a5278bf251fb1870bd6c15e6
TadhgCahill <tadhg.cahill@gmail.com>
2025-09-06 16:54:21 -0700

    Update app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -3,5 +3,8 @@ from dash import Dash
 # dash app w multi page enabled
 app = Dash(__name__, 
            use_pages=True,
-           suppress_callback_exceptions=True)
+           suppress_callback_exceptions=True, 
+           update_title=False,
+           external_scripts=["https://www.googletagmanager.com/gtag/js?id=G-TOKEN"]) #replace with real token
+
 app.title = "Energy Dashboard"
